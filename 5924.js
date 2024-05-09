150d2fded7377da6bf446b3711fc12d12a181c16
Brittany <bgw26@rutgers.edu>
2024-05-09 00:47:58 -0500

    5924

---
+++ 5924.js
@@ -0,0 +1,59 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function  makeNegative (num) {
+    if(num<0){
+        return num
+    }else{
+        return -num
+    }   
+}
+
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+// About factors
+// Factors are numbers you can multiply together to get another number.
+
+// 2 and 3 are factors of 6 because: 2 * 3 = 6
+
+// You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+// You can use the mod operator (%) in most languages to check for a remainder
+// For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+// Note: base is a non-negative number, factor is a positive number.
+
+function checkForFactor(base, factor) {
+    if(base % factor === 0){
+        return true
+    }else{
+        return false
+    }
+    
+}
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+
+function doubleChar(str){
+    
+        let doubleString = '';
+        for (let i = 0; i < str.length; i++) {
+          doubleString += str[i] + str[i]
+        }
+        return doubleString;
+}
