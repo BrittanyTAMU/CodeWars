720a34fba80601981b56aebf6b854806d591950e
Brittany <bgw26@rutgers.edu>
2024-05-14 04:17:31 -0500

    51424

---
+++ 51424.js
@@ -0,0 +1,67 @@
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+// Examples:(Input --> Output)
+
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+
+function findNextSquare(sq){
+    var root1;
+    var nextroot;
+    if(Math.sqrt(sq)%1 ===0){
+         root1=Math.sqrt(sq)
+         nextroot=root1+1
+    }else{
+        return -1;
+    }
+    return nextroot*nextroot;
+}
+
+findNextSquare(625) 
+
+// There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+
+// input
+// customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+// n: a positive integer, the number of checkout tills.
+// output
+// The function should return an integer, the total time required.
+
+// Important
+// Please look at the examples and clarifications below, to ensure you understand the task correctly :)
+
+// Examples
+// queueTime([5,3,4], 1)
+// // should return 12
+// // because when there is 1 till, the total time is just the sum of the times
+
+// queueTime([10,2,3,3], 2)
+// // should return 10
+// // because here n=2 and the 2nd, 3rd, and 4th people in the 
+// // queue finish before the 1st person has finished.
+
+// queueTime([2,3,10], 2)
+// // should return 12
+// Clarifications
+// There is only ONE queue serving many tills, and
+// The order of the queue NEVER changes, and
+// The front person in the queue (i.e. the first element in the array/list) proceeds to a till as soon as it becomes free.
+// N.B. You should assume that all the test input will be valid, as specified above.
+
+// P.S. The situation in this kata can be likened to the more-computer-science-related idea of a thread pool, with relation to running multiple processes at the same time: https://en.wikipedia.org/wiki/Thread_pool
+
+function queueTime(customers, n) {
+    var arr = new Array(n).fill(0);
+  
+    for (var i = 0; i < customers.length; i++) {
+      var idx = arr.indexOf(Math.min(...arr));
+      arr[idx] += customers[i];
+    }
+  
+    return Math.max(...arr);
+  }
